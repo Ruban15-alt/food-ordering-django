fa6cb026242137987821c444e4cd84429aa130b6
Ruban Kumar <rubansrinivas15@gmail.com>
2026-07-20 22:36:42 +0530

    Added restaurant rating

--- restaurants/models.py
+++ restaurants/models.py
@@ -3,8 +3,10 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=200, blank=True)
+    rating = models.IntegerField(default=5)
     is_open = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+    
