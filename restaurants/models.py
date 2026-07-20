ab44bbe5ed3d6f0b22c2d39630bbc275c3966a15
Ruban Kumar <rubansrinivas15@gmail.com>
2026-07-20 17:58:39 +0530

    Added address field to Restaurant model

--- restaurants/models.py
+++ restaurants/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200, blank=True)
     is_open = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
